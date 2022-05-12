9f016071467649e2718bca5a96b31ab7fba429f8
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-05-12 11:20:59 -0600

    My branch

--- __main__.py
+++ __main__.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     menu_view = MenuView()
     window.show_view(menu_view)
-    arcade.run() 
+    arcade.run()  
