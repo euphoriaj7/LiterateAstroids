2bd41b390d694fd36aa454bc2e14104473e2201e
MorganAllengit <MorganAllen@byui.edu>
2022-05-12 12:25:17 -0600

    add inputs

--- game/director.py
+++ game/director.py
@@ -2,8 +2,8 @@ import arcade
 from game.constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
-    SCREEN_TITLE
 )
+from game.actor import Actor
 
 class Director(arcade.View):
 
@@ -12,6 +12,18 @@ class Director(arcade.View):
 
     def setup(self):
         self.background = arcade.load_texture('game\images\stars.png')
+        self.spritelist = arcade.sprite_list()
+        self.actor = Actor()
+        self.spritelist.append(self.actor) # adds actor(all sprites) to sprite list
     
     def on_draw(self):
+        arcade.start_render()
         arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        self.spritelist.draw() # adds all actors to screen
+    
+    def on_update(self):
+        self.spritelist.update()
+        #update actors
+        #call check collisions
+        #update health & score
+    
