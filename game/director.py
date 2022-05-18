33f738c74fd657d9825028b36b968cb054f9344c
MorganAllengit <MorganAllen@byui.edu>
2022-05-18 13:02:27 -0600

    fixed director

--- game/director.py
+++ game/director.py
@@ -10,24 +10,25 @@ class Director(arcade.View):
 
     def __init__(self):
         super().__init__()
-        self.astroidlist = None
+        self.spritelist = None
+        self.ship = None
 
     def setup(self):
         self.background = arcade.load_texture('game/images/stars.png')
         #self.forebackground = arcade.load_texture('game\images\shipshell.png')
-        self.astroidlist = arcade.sprite_list() # creates a sprite list under name astroidlist
+        self.spritelist = arcade.SpriteList() # creates a sprite list under name spritelist
         self.ship = Ship()
-        self.astroidlist.append(self.ship) # adds actor(all sprites) to sprite list
+        self.spritelist.append(self.ship) # adds actor(all sprites) to sprite list
     
     def on_draw(self):
         arcade.start_render()
         arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH, SCREEN_HEIGHT, self.forebackground)
-        self.astroidlist.draw() # adds all actors to screen
+        self.spritelist.draw() # adds all actors to screen
         #arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         #place score and word box
     
     #def on_update(self):
-        self.astroidlist.update() # updates all actor sprites
+        #self.spritelist.update() # updates all actor sprites
         #call check collisions
         #update health & score
     
