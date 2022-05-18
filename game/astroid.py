126b008ffd7226d052782bb4559db42b684fe628
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-05-18 13:04:15 -0600

    adjusted actors

--- game/astroid.py
+++ game/astroid.py
@@ -7,9 +7,7 @@ from game.physics import Physics
 class Astroid(Actor):
 
     def __init__(self):
-        super().__init__()
-        self.set_image("images\spaceship.png")
-        self.set_position(640, 360)
+        pass
 
 
 
--- game/ship.py
+++ game/ship.py
@@ -2,11 +2,13 @@
 # - score/points
 
 from game.actor import Actor
+from game.physics import Physics
 
 class Ship(Actor):
+    def __init__(self): 
+        super().__init__()
+        self.set_image("images\spaceship.png")
+        self.set_position(640, 360)
 
-
-    def __init__(): 
-        x = 3
 
 
