11f165aa3043a25904e1f35204263df99228b3ad
MorganAllengit <MorganAllen@byui.edu>
2022-05-18 13:43:33 -0600

    ship in front of background

--- game/director.py
+++ game/director.py
@@ -15,16 +15,16 @@ class Director(arcade.View):
 
     def setup(self):
         self.background = arcade.load_texture('game/images/stars.png')
-        #self.forebackground = arcade.load_texture('game\images\shipshell.png')
+        #self.foreground = arcade.load_texture('game\images\shipshell.png')
         self.spritelist = arcade.SpriteList() # creates a sprite list under name spritelist
         self.ship = Ship()
         self.spritelist.append(self.ship) # adds actor(all sprites) to sprite list
     
     def on_draw(self):
         arcade.start_render()
-        #arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH, SCREEN_HEIGHT, self.forebackground)
-        self.spritelist.draw() # adds all actors to screen
         arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        self.spritelist.draw() # adds all actors to screen
+        #arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH, SCREEN_HEIGHT, self.foreground)
         #place score and word box
     
     #def on_update(self):
