9344c2b3d5b77aab308efcb9d8dd71391f3fb00d
Dylan Ashby <doops561@gmail.com>
2022-05-24 04:46:38 -0600

    Added capital inputs using the shift key

--- game/inputs.py
+++ game/inputs.py
@@ -14,38 +14,72 @@ class Inputs():
     # Backspace deletes the last character from the input string.
     def pressed(self, symbol, modifier):
         # Check the symbol with the alphabet and add the appropriate char
-        if symbol == arcade.key.A: self.input = self.input + 'a'
-        if symbol == arcade.key.B: self.input = self.input + 'b'
-        if symbol == arcade.key.C: self.input = self.input + 'c'
-        if symbol == arcade.key.D: self.input = self.input + 'd'
-        if symbol == arcade.key.E: self.input = self.input + 'e'
-        if symbol == arcade.key.F: self.input = self.input + 'f'
-        if symbol == arcade.key.G: self.input = self.input + 'g'
-        if symbol == arcade.key.H: self.input = self.input + 'h'
-        if symbol == arcade.key.I: self.input = self.input + 'i'
-        if symbol == arcade.key.J: self.input = self.input + 'j'
-        if symbol == arcade.key.K: self.input = self.input + 'k'
-        if symbol == arcade.key.L: self.input = self.input + 'l'
-        if symbol == arcade.key.M: self.input = self.input + 'm'
-        if symbol == arcade.key.N: self.input = self.input + 'n'
-        if symbol == arcade.key.O: self.input = self.input + 'o'
-        if symbol == arcade.key.P: self.input = self.input + 'p'
-        if symbol == arcade.key.Q: self.input = self.input + 'q'
-        if symbol == arcade.key.R: self.input = self.input + 'r'
-        if symbol == arcade.key.S: self.input = self.input + 's'
-        if symbol == arcade.key.T: self.input = self.input + 't'
-        if symbol == arcade.key.U: self.input = self.input + 'u'
-        if symbol == arcade.key.V: self.input = self.input + 'v'
-        if symbol == arcade.key.W: self.input = self.input + 'w'
-        if symbol == arcade.key.X: self.input = self.input + 'x'
-        if symbol == arcade.key.Y: self.input = self.input + 'y'
-        if symbol == arcade.key.Z: self.input = self.input + 'z'
+
+        # UPPERCASE
+        if modifier & arcade.key.MOD_SHIFT:
+            if symbol == arcade.key.A: self.input = self.input + 'A'
+            if symbol == arcade.key.B: self.input = self.input + 'B'
+            if symbol == arcade.key.C: self.input = self.input + 'C'
+            if symbol == arcade.key.D: self.input = self.input + 'D'
+            if symbol == arcade.key.E: self.input = self.input + 'E'
+            if symbol == arcade.key.F: self.input = self.input + 'F'
+            if symbol == arcade.key.G: self.input = self.input + 'G'
+            if symbol == arcade.key.H: self.input = self.input + 'H'
+            if symbol == arcade.key.I: self.input = self.input + 'I'
+            if symbol == arcade.key.J: self.input = self.input + 'J'
+            if symbol == arcade.key.K: self.input = self.input + 'K'
+            if symbol == arcade.key.L: self.input = self.input + 'L'
+            if symbol == arcade.key.M: self.input = self.input + 'M'
+            if symbol == arcade.key.N: self.input = self.input + 'N'
+            if symbol == arcade.key.O: self.input = self.input + 'O'
+            if symbol == arcade.key.P: self.input = self.input + 'P'
+            if symbol == arcade.key.Q: self.input = self.input + 'Q'
+            if symbol == arcade.key.R: self.input = self.input + 'R'
+            if symbol == arcade.key.S: self.input = self.input + 'S'
+            if symbol == arcade.key.T: self.input = self.input + 'T'
+            if symbol == arcade.key.U: self.input = self.input + 'U'
+            if symbol == arcade.key.V: self.input = self.input + 'V'
+            if symbol == arcade.key.W: self.input = self.input + 'W'
+            if symbol == arcade.key.X: self.input = self.input + 'X'
+            if symbol == arcade.key.Y: self.input = self.input + 'Y'
+            if symbol == arcade.key.Z: self.input = self.input + 'Z'
+
+        # LOWERCASE
+        else:
+            if symbol == arcade.key.A: self.input = self.input + 'a'
+            if symbol == arcade.key.B: self.input = self.input + 'b'
+            if symbol == arcade.key.C: self.input = self.input + 'c'
+            if symbol == arcade.key.D: self.input = self.input + 'd'
+            if symbol == arcade.key.E: self.input = self.input + 'e'
+            if symbol == arcade.key.F: self.input = self.input + 'f'
+            if symbol == arcade.key.G: self.input = self.input + 'g'
+            if symbol == arcade.key.H: self.input = self.input + 'h'
+            if symbol == arcade.key.I: self.input = self.input + 'i'
+            if symbol == arcade.key.J: self.input = self.input + 'j'
+            if symbol == arcade.key.K: self.input = self.input + 'k'
+            if symbol == arcade.key.L: self.input = self.input + 'l'
+            if symbol == arcade.key.M: self.input = self.input + 'm'
+            if symbol == arcade.key.N: self.input = self.input + 'n'
+            if symbol == arcade.key.O: self.input = self.input + 'o'
+            if symbol == arcade.key.P: self.input = self.input + 'p'
+            if symbol == arcade.key.Q: self.input = self.input + 'q'
+            if symbol == arcade.key.R: self.input = self.input + 'r'
+            if symbol == arcade.key.S: self.input = self.input + 's'
+            if symbol == arcade.key.T: self.input = self.input + 't'
+            if symbol == arcade.key.U: self.input = self.input + 'u'
+            if symbol == arcade.key.V: self.input = self.input + 'v'
+            if symbol == arcade.key.W: self.input = self.input + 'w'
+            if symbol == arcade.key.X: self.input = self.input + 'x'
+            if symbol == arcade.key.Y: self.input = self.input + 'y'
+            if symbol == arcade.key.Z: self.input = self.input + 'z'
+
+        # On SPACE, append the space character to the input string
         if symbol == arcade.key.SPACE: self.input = self.input + ' '
         
         # If enter, check vs active_word
         # Clear input and return true if they match
         if symbol == arcade.key.ENTER:
-            if (self.active_word == self.input):
+            if self.active_word == self.input:
                 self.input = ""
                 print (self.input)  # TESTING LINE
                 return True
