1ff758f748bc0faaeb10a206a107a2635a054fdc
Dylan Ashby <doops561@gmail.com>
2022-05-24 03:32:59 -0600

    Got the asteroid spawning code to work!

--- game/astroid.py
+++ game/astroid.py
@@ -1,6 +1,9 @@
 # functions 
 # - words and get/setter
+from os import O_TEXT
 from game.constants import (
+    CENTER_X,
+    CENTER_Y,
     SHIP_SCALE,
     SCREEN_HEIGHT,
     SCREEN_WIDTH
@@ -15,20 +18,18 @@ class Astroid(arcade.Sprite):
         super().__init__("game/images/meteor1.png", SHIP_SCALE)
         self.center_x = SCREEN_WIDTH / 3
         self.center_y = SCREEN_HEIGHT / 3
+        self.physics = Physics(x, y, deltaX, deltaY, 0)
         self.getLetter()
         self.draw()
-# THIS IS NEW CODE RELATED TO ASTEROID SPAWNING
-        self.physics = Physics(x, y, deltaX, deltaY)
 
-# to get letter to print in terminal
+    # to get letter to print in terminal
     def getLetter(self):
         data = Data() 
         letter_asteroid = data.random_word()
         self.text = letter_asteroid
         print(letter_asteroid)
-        
+
     def draw(self):
-        print("Draw astroid")
         arcade.draw_text(
             "fish sticks",
             SCREEN_WIDTH / 4,
@@ -36,11 +37,7 @@ class Astroid(arcade.Sprite):
             arcade.csscolor.RED,
             25
             )
+        arcade.draw_circle_filled(self.physics.get_pos()[0], self.physics.get_pos()[1], 10, arcade.color.RED)
     
-# ALSO UPDATED FOR ASTEROID SPAWNING
     def update(self):
         self.physics.tick_update()
-
-        # POSITIONAL TEST PRINTS
-        print("ASTEROID POSITIONAL TEST")
-        print(self.physics.get_pos)
--- game/director.py
+++ game/director.py
@@ -1,4 +1,4 @@
-from LiterateAstroids.game.constants import CENTER_X, CENTER_Y
+#from LiterateAstroids.game.constants import CENTER_X, CENTER_Y
 import arcade
 import math
 import random
@@ -6,6 +6,8 @@ import random
 from game.constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
+    CENTER_X,
+    CENTER_Y,
 )
 from game.astroid import Astroid
 from game.ship import Ship
@@ -24,7 +26,7 @@ class Director(arcade.View):
         self.foreground = arcade.load_texture('game/images/shipshell.png')
         self.spritelist = arcade.SpriteList() # creates a sprite list under name spritelist
         self.ship = Ship()
-        #self.astroid = Astroid()
+        #self.astroid = Astroid(0, 0, 0 ,0)
         
         # TESTING SPAWN ASTEROID CODE
         self.astroid = self.spawn_asteroid(2)
@@ -35,11 +37,14 @@ class Director(arcade.View):
     def on_draw(self):
         arcade.start_render()
         arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-        self.spritelist.draw() # adds all actors to screen
+        #self.spritelist.draw() # adds all actors to screen
         arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH, SCREEN_HEIGHT, self.foreground)
+
+        # Updates graphics for all actors // THIS IS ADDED IN AS A FIX TO self.spritelist.draw()
+        for sprite in self.spritelist: sprite.draw()
         #place score and word box
     
-    def on_update(self):
+    def on_update(self, delta_time):
         self.spritelist.update() # updates all sprites
         #call check collisions
         #update health & score
@@ -52,16 +57,16 @@ class Director(arcade.View):
         side = random.randint(1,2)
 
         # Set a random spawn angle based on the side selected
-        if (side == 1): theta = random.randrange(-1*math.pi, math.pi)
-        else:           theta = random.randrange(-1*math.pi, math.pi)
+        if (side == 1): theta = random.uniform(math.atan2(-SCREEN_WIDTH,-SCREEN_HEIGHT), math.atan2(-SCREEN_WIDTH,SCREEN_HEIGHT))
+        else:           theta = random.uniform(math.atan2(SCREEN_WIDTH,-SCREEN_HEIGHT), math.atan2(SCREEN_WIDTH,SCREEN_HEIGHT))
 
         # Set the spawn coordinates based on the spawn angle
-        x = (CENTER_X + 100 * math.cos(theta)) - (100 * math.sin(theta))
-        y = (CENTER_Y + 100 * math.cos(theta)) + (100 * math.sin(theta))
+        x = CENTER_X - (CENTER_X * math.sin(theta))
+        y = CENTER_Y + CENTER_X * math.cos(theta)
 
         # Set the velocity vector based on the spawn angle + 180 degrees
-        deltaX = speed * math.cos(theta + math.pi)
-        deltaY = speed * math.sin(theta + math.pi)
+        deltaX = math.cos(theta - (math.pi/2))
+        deltaY = math.sin(theta - (math.pi/2))
 
         # Return a new asteroid object
         return Astroid(x, y, deltaX, deltaY)
