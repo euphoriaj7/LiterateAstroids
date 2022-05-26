178602901f159ab49306301642e359b090a4082a
Dylan Ashby <doops561@gmail.com>
2022-05-26 03:23:46 -0600

    Added in functionality for "get_pos", made update move positional values from physics to the sprite, added in a test graphic

--- game/laser.py
+++ game/laser.py
@@ -11,6 +11,7 @@ from game.constants import (
 from game.actor import Actor
 from game.physics import Physics
 import arcade
+import math
 
 class Laser(arcade.Sprite):
     def __init__(self, x, y, deltaX, deltaY, rotation): 
@@ -19,12 +20,20 @@ class Laser(arcade.Sprite):
         self.center_y = SCREEN_HEIGHT / 3
         self.physics = Physics(x, y, deltaX, deltaY, rotation)
         self.draw()
+
+    # \\\ GET POS ///
+    # Returns the current (x, y) coordinates of the ship
+    def get_pos(self): return self.physics.get_pos()
     
     def draw(self):
-        arcade.draw_circle_filled(
-            self.physics.get_pos()[0],
-            self.physics.get_pos()[1],
-            10,
-            arcade.color.BLUE)
+        arcade.draw_line(
+            self.center_x, 
+            self.center_y, 
+            self.center_x - 150 * math.sin(self.physics.get_rotation()),
+            self.center_y + 150 * math.cos(self.physics.get_rotation()),
+            arcade.color.BLUE,5)
 
-    def update(self): self.physics.tick_update()
+    def update(self):
+        self.center_x = self.physics.get_pos()[0]   # set the physics x position over to the sprite's x positon
+        self.center_y = self.physics.get_pos()[1]   # set the physics y position over to the sprite's y positon
+        self.physics.tick_update()
