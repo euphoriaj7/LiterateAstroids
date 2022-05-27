325446117d75c7bb60282adf9522de6ec939cac9
Dylan Ashby <doops561@gmail.com>
2022-05-27 12:51:27 -0600

    Deleted extra whitespace

--- game/data.py
+++ game/data.py
@@ -18,7 +18,4 @@ class Data():
         self.read_words()
         random_numer_word = random.randrange(0,25)
         letter = self.list_of_words[random_numer_word]
-        return letter
-
-
-    
+        return letter    
