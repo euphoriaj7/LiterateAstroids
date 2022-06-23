4eece8a842e48cda1ef3f7f79e68c44bc42fcca9
Jeff Pitcher <pit19004@byui.edu>
2022-06-23 11:43:02 -0600

    updating fonts

--- game/gameover.py
+++ game/gameover.py
@@ -3,7 +3,7 @@ from game.constants import (
     WORKING_DIRECTORY,
     SCREEN_HEIGHT,
     SCREEN_WIDTH,
-    FONT
+    # FONT
 )
 
 class GameOver(arcade.View):
@@ -34,15 +34,15 @@ class GameOver(arcade.View):
             0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.end)
 
         arcade.draw_text("Literate Astroids", SCREEN_WIDTH / 3,
-                         SCREEN_HEIGHT - SCREEN_HEIGHT / 6, arcade.color.WHITE, 50, font_name=self.FONT)
+                         SCREEN_HEIGHT - SCREEN_HEIGHT / 6, arcade.color.WHITE, 50, font_name="Kenny Pixel")
         arcade.draw_text("Final Score: "+ str(self.fscore), SCREEN_WIDTH /
-                         2, SCREEN_HEIGHT - SCREEN_HEIGHT / 3, arcade.color.WHITE, 40, font_name=self.FONT)
+                         2, SCREEN_HEIGHT - SCREEN_HEIGHT / 3, arcade.color.WHITE, 40, font_name="Kenny Pixel")
         arcade.draw_text("High Scores", SCREEN_WIDTH / 13,
-                         SCREEN_HEIGHT - SCREEN_HEIGHT / 3, arcade.color.WHITE, 40, font_name=self.FONT)
+                         SCREEN_HEIGHT - SCREEN_HEIGHT / 3, arcade.color.WHITE, 40, font_name="Kenny Pixel")
         arcade.draw_text("Add Name: "+ str(self.inputs.get_input()), SCREEN_WIDTH / 2,
-                    SCREEN_HEIGHT / 2, arcade.color.WHITE, 40, font_name=self.FONT)
+                    SCREEN_HEIGHT / 2, arcade.color.WHITE, 40, font_name="Kenny Pixel")
         arcade.draw_text(self.result, SCREEN_WIDTH / 2,
-                    SCREEN_HEIGHT / 4, arcade.color.WHITE, 40, font_name=self.FONT)
+                    SCREEN_HEIGHT / 4, arcade.color.WHITE, 40, font_name="Kenny Pixel")
     
     
         
--- game/inputs.py
+++ game/inputs.py
@@ -1,4 +1,4 @@
-from tkinter.font import Font
+# from tkinter.font import Font
 from game.data import Data
 from game.constants import (
     SCREEN_WIDTH,
@@ -6,7 +6,7 @@ from game.constants import (
     SHIP_SCALE,
     CENTER_X,
     CENTER_Y,
-    FONT
+    # FONT
 )
 
 import arcade
@@ -20,7 +20,7 @@ class Inputs(arcade.Sprite):
         self.input = ""
         self.is_backspace = False
         self.backspace_counter = 0
-        self.FONT = "Kenny Pixel"
+        # self.FONT = "Kenny Pixel"
 
     def get_input(self):
         return self.input
@@ -34,14 +34,14 @@ class Inputs(arcade.Sprite):
             CENTER_X - 200,
             CENTER_Y - 300,
             arcade.color.GREEN,
-            30, font_name=self.FONT)
+            30, font_name="Kenny Pixel")
         # Display active word
         arcade.draw_text(
             score,
             CENTER_X + 320,
             CENTER_Y - 335,
             arcade.color.GREEN,
-            30, font_name=self.FONT)
+            30, font_name="Kenny Pixel")
     
     # \\\ Update ///
     # Watches for signals related to "held down" keys and performs related code
