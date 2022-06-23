dab67e324535f0e3cf789fca86f5ff4085777662
Jeff Pitcher <pit19004@byui.edu>
2022-06-23 12:22:34 -0600

    trying to change text just on the game over screen

--- game/gameover.py
+++ game/gameover.py
@@ -32,15 +32,15 @@ class GameOver(arcade.View):
             0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.end)
 
         arcade.draw_text("Literate Astroids", SCREEN_WIDTH / 3,
-                         SCREEN_HEIGHT - SCREEN_HEIGHT / 6, arcade.color.WHITE, 50)
+                         SCREEN_HEIGHT - SCREEN_HEIGHT / 6, arcade.color.WHITE, 50, font_name="Kenny Pixel Square")
         arcade.draw_text("Final Score: "+ str(self.fscore), SCREEN_WIDTH /
-                         2, SCREEN_HEIGHT - SCREEN_HEIGHT / 3, arcade.color.WHITE, 40)
+                         2, SCREEN_HEIGHT - SCREEN_HEIGHT / 3, arcade.color.WHITE, 40, font_name="Kenny Pixel Square")
         arcade.draw_text("High Scores", SCREEN_WIDTH / 13,
-                         SCREEN_HEIGHT - SCREEN_HEIGHT / 3, arcade.color.WHITE, 40)
+                         SCREEN_HEIGHT - SCREEN_HEIGHT / 3, arcade.color.WHITE, 40, font_name="Kenny Pixel Square")
         arcade.draw_text("Add Name: "+ str(self.inputs.get_input()), SCREEN_WIDTH / 2,
-                    SCREEN_HEIGHT / 2, arcade.color.WHITE, 40)
+                    SCREEN_HEIGHT / 2, arcade.color.WHITE, 40, font_name="Kenny Pixel Square")
         arcade.draw_text(self.result, SCREEN_WIDTH / 2,
-                    SCREEN_HEIGHT / 4, arcade.color.WHITE, 40)
+                    SCREEN_HEIGHT / 4, arcade.color.WHITE, 40, font_name="Kenny Pixel Square")
     
     
         
